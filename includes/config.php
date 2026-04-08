78144f95accb438ec1168edf2d4ab229837fb817
Vladislav <vladsahar27@gmail.com>
2026-04-07 21:44:00 -0400

    Fixes with the database connection

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_HOST',     getenv('MYSQLHOST')     ?: 'localhost');
-define('DB_PORT',     getenv('MYSQLPORT')     ?: '3306');
-define('DB_NAME',     getenv('MYSQLDATABASE') ?: 'railway');
-define('DB_USER',     getenv('MYSQLUSER')     ?: 'root');
-define('DB_PASS',     getenv('MYSQLPASSWORD') ?: '');
+define('DB_HOST',  $_ENV['MYSQLHOST']     ?? 'localhost');
+define('DB_PORT',  $_ENV['MYSQLPORT']     ?? '3306');
+define('DB_NAME',  $_ENV['MYSQLDATABASE'] ?? 'railway');
+define('DB_USER',  $_ENV['MYSQLUSER']     ?? 'root');
+define('DB_PASS',  $_ENV['MYSQLPASSWORD'] ?? '');
