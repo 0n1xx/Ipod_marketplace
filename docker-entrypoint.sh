89771897fb03360a7b05d2d42886cef5e5a2eb6a
Vladislav <vladsahar27@gmail.com>
2026-04-07 23:03:14 -0400

    Another small data fixes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 set -e
 
-# Use PORT from env, fallback to 80
 APP_PORT="${PORT:-80}"
 
-# Fix Apache port
-sed -i "s/Listen 80/Listen ${APP_PORT}/g" /etc/apache2/ports.conf
+# Rewrite ports.conf directly instead of using sed
+echo "Listen ${APP_PORT}" > /etc/apache2/ports.conf
+
+# Fix VirtualHost port
 sed -i "s/:80>/:${APP_PORT}>/g" /etc/apache2/sites-enabled/000-default.conf
 
 # PHP settings
 echo "variables_order = EGPCS" >> /etc/php/8.1/apache2/php.ini
 
-# MySQL env vars
+# Env vars for Apache/PHP
 echo "SetEnv MYSQLHOST ${MYSQLHOST}" >> /etc/apache2/apache2.conf
 echo "SetEnv MYSQLPORT ${MYSQLPORT}" >> /etc/apache2/apache2.conf
 echo "SetEnv MYSQLDATABASE ${MYSQLDATABASE}" >> /etc/apache2/apache2.conf
 echo "SetEnv MYSQLUSER ${MYSQLUSER}" >> /etc/apache2/apache2.conf
 echo "SetEnv MYSQLPASSWORD ${MYSQLPASSWORD}" >> /etc/apache2/apache2.conf
-
-# Cloudinary env vars
 echo "SetEnv CLOUDINARY_CLOUD_NAME ${CLOUDINARY_CLOUD_NAME}" >> /etc/apache2/apache2.conf
 echo "SetEnv CLOUDINARY_API_KEY ${CLOUDINARY_API_KEY}" >> /etc/apache2/apache2.conf
 echo "SetEnv CLOUDINARY_API_SECRET ${CLOUDINARY_API_SECRET}" >> /etc/apache2/apache2.conf
