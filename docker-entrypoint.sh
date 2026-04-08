eab0af4265a2b7658bb8bf4c03cae6e16aa1ceea
Vladislav <vladsahar27@gmail.com>
2026-04-07 23:06:52 -0400

    Another small data fixes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,23 +3,20 @@ set -e
 
 APP_PORT="${PORT:-80}"
 
-# Rewrite ports.conf directly instead of using sed
+# Fix Apache port
 echo "Listen ${APP_PORT}" > /etc/apache2/ports.conf
-
-# Fix VirtualHost port
 sed -i "s/:80>/:${APP_PORT}>/g" /etc/apache2/sites-enabled/000-default.conf
 
-# PHP settings
-echo "variables_order = EGPCS" >> /etc/php/8.1/apache2/php.ini
-
-# Env vars for Apache/PHP
-echo "SetEnv MYSQLHOST ${MYSQLHOST}" >> /etc/apache2/apache2.conf
-echo "SetEnv MYSQLPORT ${MYSQLPORT}" >> /etc/apache2/apache2.conf
-echo "SetEnv MYSQLDATABASE ${MYSQLDATABASE}" >> /etc/apache2/apache2.conf
-echo "SetEnv MYSQLUSER ${MYSQLUSER}" >> /etc/apache2/apache2.conf
-echo "SetEnv MYSQLPASSWORD ${MYSQLPASSWORD}" >> /etc/apache2/apache2.conf
-echo "SetEnv CLOUDINARY_CLOUD_NAME ${CLOUDINARY_CLOUD_NAME}" >> /etc/apache2/apache2.conf
-echo "SetEnv CLOUDINARY_API_KEY ${CLOUDINARY_API_KEY}" >> /etc/apache2/apache2.conf
-echo "SetEnv CLOUDINARY_API_SECRET ${CLOUDINARY_API_SECRET}" >> /etc/apache2/apache2.conf
+# Pass ALL env vars to PHP via php.ini (avoids SetEnv Apache issues)
+PHP_INI="/etc/php/8.1/apache2/php.ini"
+echo "variables_order = EGPCS" >> "$PHP_INI"
+echo "env[MYSQLHOST] = \"${MYSQLHOST}\"" >> "$PHP_INI"
+echo "env[MYSQLPORT] = \"${MYSQLPORT}\"" >> "$PHP_INI"
+echo "env[MYSQLDATABASE] = \"${MYSQLDATABASE}\"" >> "$PHP_INI"
+echo "env[MYSQLUSER] = \"${MYSQLUSER}\"" >> "$PHP_INI"
+echo "env[MYSQLPASSWORD] = \"${MYSQLPASSWORD}\"" >> "$PHP_INI"
+echo "env[CLOUDINARY_CLOUD_NAME] = \"${CLOUDINARY_CLOUD_NAME}\"" >> "$PHP_INI"
+echo "env[CLOUDINARY_API_KEY] = \"${CLOUDINARY_API_KEY}\"" >> "$PHP_INI"
+echo "env[CLOUDINARY_API_SECRET] = \"${CLOUDINARY_API_SECRET}\"" >> "$PHP_INI"
 
 exec apache2ctl -D FOREGROUND
